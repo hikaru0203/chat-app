e97549b69576ec254dca158094c4a2e17cf98d8b
hikaru0203 <h2.soccer.3h@i.softbank.jp>
2021-03-03 19:31:26 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,17 @@
-class MessageController < ApplicationController
+class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
+  end
 
-    def create
-      @room = Room.find(params[:room_id])
+  def create
+    @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      messages = @room.messages.include(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
@@ -18,6 +19,7 @@ class MessageController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
+
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,25 +2,32 @@ class RoomsController < ApplicationController
 
   def index
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+      if @room.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
   
-def new
-  @room = Room.new
 end
+# @room = Room.new(room_params)
+#     if @room.save
+#       redirect_to root_path
+#     else
+#       render :new
+#     end
+#   end
 
-def create
-  @room = Room.new(room_params)
-    if @room.save
-      redirect_to root_path
-    else
-      render :new
-    end
-end
-
-@room = Room.new(room_params)
-    if @room.save
-      redirect_to root_path
-    else
-      render :new
-    end
-
-end
